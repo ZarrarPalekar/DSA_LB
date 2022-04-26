867ce1ccf1bfd22902cf87478b447e32abff9d21
Zarrar Jawed Ahmad Palekar <zarrar.palekar@syndicationpro.com>
2022-04-26 23:04:15 +0530

    array Problems initial commit

--- arrayProblems.cpp
+++ arrayProblems.cpp
@@ -15,14 +15,35 @@ void reverseArray(int arr[], int num)
     }
 }
 
+bool isPalindrome(int arr[], int num)
+{
+    int i = 0;
+    int j = num - 1;
+    while (i < j)
+    {
+        if (arr[i] == arr[j])
+        {
+            i++;
+            j--;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6};
-    reverseArray(arr, 6);
+    int arr[] = {1, 2, 3, 3, 2, 1};
+    // reverseArray(arr, 6);
 
-    for (int i = 0; i < 6; i++)
-    {
-        cout << arr[i];
-    }
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout << arr[i];
+    // }
+
+    cout << isPalindrome(arr, sizeof(arr) / sizeof(arr[0]));
     cout << endl;
 }
