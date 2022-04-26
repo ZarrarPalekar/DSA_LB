db6d2820d37659d1ed76dd47c03998fe5c595211
Zarrar Jawed Ahmad Palekar <zarrar.palekar@syndicationpro.com>
2022-04-26 21:32:40 +0530

    .vscode remove

--- arrayProblems.cpp
+++ arrayProblems.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+
 // reverse an array
+
+void reverseArray(int arr[], int num)
+{
+    int i = 0;
+    int j = num - 1;
+    while (i < j)
+    {
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    reverseArray(arr, 6);
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i];
+    }
+    cout << endl;
+}
